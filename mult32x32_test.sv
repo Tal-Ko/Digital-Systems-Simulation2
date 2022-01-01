28af5023cd78e77b90c5113a02371df0586cfc29
Tal Kogan <tal.kogan@campus.technion.ac.il>
2022-01-01 19:35:47 +0200

    stages: wet 3.4: Add testbench for FSM & Arith unit

--- mult32x32_test.sv
+++ mult32x32_test.sv
@@ -12,6 +12,44 @@ module mult32x32_test;
 // Put your code here
 // ------------------
 
+    mult32x32 uut (
+        .clk(clk),
+        .reset(reset),
+        .start(start),
+        .a(a),
+        .b(b),
+        .busy(busy),
+        .product(product)
+    );
+
+    initial begin
+        clk = 1'b1;
+        reset = 1'b1;
+        start = 1'b0;
+        a = 64'b0;
+        b = 64'b0;
+
+        repeat (4) begin
+            @(posedge clk);
+        end
+
+        reset = 1'b0;
+        @(posedge clk);
+
+        a = 207016353; // 00001100010101101101000110100001
+        b = 207905126; // 00001100011001000110000101100110
+
+        start = 1'b1;
+        @(posedge clk);
+        start = 1'b0;
+
+        @(negedge busy);
+    end
+
+    always begin
+        #1 clk = ~clk;
+    end
+
 // End of your code
 
 endmodule
